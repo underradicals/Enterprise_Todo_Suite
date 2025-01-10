a428ef2d71063456865bf1d0563e4eb2ec9bfa18
Joseph <underradicals@gmail.com>
2025-01-10 08:19:33 -0600

    Add scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Enterprise_Todo_Suite
-A collection of Todo Apps in different Tech Stacks
+# Todo Suite
+A collection of Todo Apps in different Tech Stacks. This is the quintessential collection of Todo App implementations. 
+
+## Front End Projects
+- [ ] Vanilla Javascript (Typescript)
+- [ ] Nodejs (Typescript, Express)
+- [ ] .NET Core (Razor Pages)
+- [ ] React
+- [ ] Vue
+- [ ] Angular
+
+## Backend Projects
+- [ ] Nodejs (Typescript)
+- [ ] .NET Core (Web API)
+- [ ] Python (FastAPI)
+- [ ] Scala (JVM)
+- [ ] Spring Boot (JVM)
+
+## Stores
+- [ ] Flat File
+- [ ] SQLite
+- [ ] Postgres
+- [ ] MSSQL Server
+- [ ] MySQL
+
+## CSS
+- [ ] Vanilla CSS (Sass)
+- [ ] Tailwind
+- [ ] Bootstrap
---
+++ Todo_TsClient/vite.config.ts
@@ -0,0 +1,52 @@
+﻿import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
+// @ts-ignore
+import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
+
+interface HandlebarsDataOptions {
+    [key: string]: {title: string};
+}
+
+const handlebarsPageData: HandlebarsDataOptions = {
+    '/index.html': {
+        title: 'Home'
+    },
+}
+
+export default defineConfig({
+    plugins: [handlebars({
+        reloadOnPartialChange: true,
+        partialDirectory: resolve(__dirname, 'partials'),
+        context(pagePath: string) {
+            return handlebarsPageData[pagePath]
+        }
+    })],
+    build: {
+        outDir: './build',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                admin: resolve(__dirname, 'admin/index.html'),
+                login: resolve(__dirname, 'login/index.html'),
+                register: resolve(__dirname, 'register/index.html'),
+                dashboard: resolve(__dirname, 'dashboard/index.html'),
+                trash: resolve(__dirname, 'trash/index.html'),
+                settings: resolve(__dirname, 'settings/index.html')
+            }
+        }
+    },
+    server: {
+        port: 8080,
+        host: '127.0.0.1',
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer]
+        },
+        modules: {
+            exportGlobals: true,
+            localsConvention: 'camelCaseOnly',
+        }
+    }
+})
